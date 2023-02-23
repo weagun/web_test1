ec98adb99c9db51ba27102218416cc2b6f81ad23
weagun <weagun@hotmail.com>
2023-02-23 15:29:42 +0800

    增加內容

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// version:0.01
+const express = require('express');
+const app = express();
+const port = 8002;
+
+console.log("server.js execute")
+console.log(app)
+var counter = 0
+
+app.get('/', (req, res) => {
+  res.send('The server is working!:' + counter);
+  counter ++
+});
+
+app.listen(port, () => {
+  console.log(`server is listening on ${port} !!!`);
+});
